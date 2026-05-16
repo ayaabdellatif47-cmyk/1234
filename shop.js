c8e0e866b11ba2347d3f213046994c93f208fa9c
ayaabdellatif47-cmyk <ayaabdellatif47@gmail.com>
2026-05-16 09:47:57 +0300

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,10 @@
+const menuBtn = document.getElementById("menuBtn");
+const sideMenu = document.getElementById("sideMenu");
+
+/* Open & Close Menu */
+
+menuBtn.onclick = function () {
+
+    sideMenu.classList.toggle("active");
+
+};
